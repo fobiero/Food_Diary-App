b82360b3d6b57866c117aabd8c147cd0d6cba8e2
feLycks <felix.obiero@student.moringaschool.com>
2021-12-01 23:42:44 +0300

    reg build complete

--- user.js
+++ user.js
@@ -1,12 +1,17 @@
 const express = require('express');
-const app = express()
+const app = express();
 const path = require('path');
 
 app.use(express.static(path.join(__dirname, './static')));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './static/user.html'));
-})
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, './static/register.html'));
+});
+
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`listening on port ${port}`));
