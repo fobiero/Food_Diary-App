1f2aaeb2c81a04c055ed54ccc6fc244bfe3dbfe8
DavidKibetRono <david.rono@student.moringaschool.com>
2021-12-01 20:53:40 +0300

    add database connection details

--- connect.php
+++ connect.php
@@ -8,9 +8,23 @@ $username="";
 
 $password="";
 
-$db="Diary";
+$db="";
+
+try{
 
 
+$dsn="mysql:host=$servername;dbname=$db";
 
+$conn=new PDO($dsn,$username,$password);
+
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+
+
+}catch(PDOEception $e){
+
+    echo 'Failed to connect'.$e->getMessage();
+}
 
 ?>
