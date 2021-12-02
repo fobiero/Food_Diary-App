a7b757df4fe54241acb5a647b1e5dd228eacf64c
feLycks <felix.obiero@student.moringaschool.com>
2021-12-02 12:58:31 +0300

    api development

--- user.js
+++ user.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const Datastore = require('nedb');
+const { request } = require('express');
 
 app.use(express.static(path.join(__dirname, './static')));
 
@@ -15,6 +17,16 @@ app.get('/register', (req, res) => {
 app.get('/dashboard', (req, res) => {
     res.sendFile(path.join(__dirname, './static/dashboard.html'));
 });
+
+app.post('/users', (req, res) => {
+        const data = request.body;
+        database.insert(data);
+    })
+    // datastore 
+const database = new Datastore('database.db');
+database.loadDatabase();
+
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`listening on port ${port}`));
